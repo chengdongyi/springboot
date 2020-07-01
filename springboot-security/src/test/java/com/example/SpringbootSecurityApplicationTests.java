98d4db510a048604d705e55000b8f524ffdbf027
chengdongyi <13088805160@163.com>
2020-07-01 17:40:29 +0800

    Spring Boot Security

---
+++ springboot-security/src/test/java/com/example/SpringbootSecurityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
