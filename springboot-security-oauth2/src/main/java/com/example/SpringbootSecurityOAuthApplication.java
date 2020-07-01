f0382987875661ea0806a6c0e16e2119edc3f9be
chengdongyi <cheng.123>
2020-07-01 22:22:23 +0800

    Spring Boot 整合 Spring Security OAuth2

---
+++ springboot-security-oauth2/src/main/java/com/example/SpringbootSecurityOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityOAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityOAuthApplication.class, args);
+    }
+
+}
