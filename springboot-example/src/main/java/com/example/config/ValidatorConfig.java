066902e9c951aba1effe41c0f46500d93488a5a0
chengdongyi <13088805160@163.com>
2020-09-17 18:04:15 +0800

    【参数校验】-快速失败(Fail Fast)

---
+++ springboot-example/src/main/java/com/example/config/ValidatorConfig.java
@@ -0,0 +1,28 @@
+package com.example.config;
+
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+/**
+ * @description: 参数校验-快速失败(Fail Fast)
+ * @author chengdongyi
+ * @date 2020/7/14 20:21
+ */
+@Configuration
+public class ValidatorConfig {
+
+    @Bean
+    public Validator validator() {
+        ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class)
+                .configure()
+                .failFast(true)
+                .buildValidatorFactory();
+        return validatorFactory.getValidator();
+    }
+
+}
