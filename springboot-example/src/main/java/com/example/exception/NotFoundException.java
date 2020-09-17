94f0b0966ec41bea4c40cc5fe3b7ffc7b34c3b10
chengdongyi <13088805160@163.com>
2020-09-17 18:06:00 +0800

    【全局异常处理】-404 处理

---
+++ springboot-example/src/main/java/com/example/exception/NotFoundException.java
@@ -0,0 +1,32 @@
+package com.example.exception;
+
+import com.example.domain.Result;
+import com.example.enums.ReturnCode;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: 404 异常处理
+ * @author chengdongyi
+ * @date 2020/9/14 14:53
+ */
+@RestController
+public class NotFoundException
+//        implements ErrorController
+
+{
+
+//    private static final String ERROR_PATH = "/error";
+//
+//    @Override
+//    public String getErrorPath() {
+//        return ERROR_PATH;
+//    }
+//
+//    @RequestMapping(ERROR_PATH)
+//    public Result error(){
+//        return Result.fail(ReturnCode.NOT_FOUND);
+//    }
+
+}
